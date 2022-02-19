0de6d7a8a289c8ca8764050e4fe4e6dcf78e1598
Yuta <y.sugiyama54@gmail.com>
2022-02-19 18:21:50 +0900

    フロントページの実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">Go Wath a Movie!</h1>
+          <hr className='mb-3'></hr>
+        </div>
+
+        <div className='row'>
+          <div className='col-md-2'>
+            <nav>
+              <ul className='list-group'>
+                <li className='list-group-item'>
+                  <Link to="/">Home</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to="/admin">Manage Catalogue</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className='col-md-10'>
+            <Routes>
+              <Route path='/movies' element={Movies}/>
+              <Route path='/admin' element={Admin}/>
+              <Route path='/' element={Home}/>
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );  
 }
 
-export default App;
+function Home() {
+  return <p>a</p>
+}
+
+function Admin() {
+  return <h2>Admin</h2>
+}
+
+function Movies() {
+  return <h2>Movies</h2>
+}
+
