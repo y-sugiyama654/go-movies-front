363c8184e5c078cc7751022238ee81c6f86721af
Yuta <y.sugiyama54@gmail.com>
2022-02-26 16:51:04 +0900

    error処理の追加

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,22 +6,41 @@ export default class Movies extends Component {
     state = { 
         movies: [],
         isLoaded: false,
+        error: null,
     };
 
     componentDidMount() {
         fetch("http://localhost:4000/v1/movies")
-            .then((response) => response.json())
+            // .then((response) => response.json())
+            .then((response) => {
+                console.log("status code is", response.status);
+                if (response.status !== "200") {
+                    let err = Error;
+                    err.message = "Invalid response code:" + response.status;
+                    this.setState({error: err});
+                }
+                return response.json();
+            })
             .then((json) => {
                 this.setState ({
                     movies: json.movies,
                     isLoaded: true,
-                })
-            })
+                }, 
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+                );
+            });
     }
 
     render() {
-        const { movies, isLoaded } = this.state
-        if (!isLoaded) {
+        const { movies, isLoaded, error } = this.state
+        if (error) {
+            return <p>Error: { error.message }</p>
+        } else if (!isLoaded) {
             return <p>Loading....</p>
         } else {
             return (
