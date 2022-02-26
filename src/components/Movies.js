5bb308c9e0d05a443949d018f6d73acd5030d5f8
Yuta <y.sugiyama54@gmail.com>
2022-02-26 16:40:10 +0900

    MoviesリストをAPIから取得

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,31 +3,40 @@ import { Link} from 'react-router-dom';
 
 export default class Movies extends Component {
 
-    state = { movies: []}
+    state = { 
+        movies: [],
+        isLoaded: false,
+    };
 
     componentDidMount() {
-        this.setState({
-            movies: [
-                { id: 1, title: "mid90s", runtime: 142 },
-                { id: 2, title: "The Usual Suspects", runtime: 120 },
-                { id: 3, title: "Forest Gump", runtime: 249 },
-            ]
-        })
+        fetch("http://localhost:4000/v1/movies")
+            .then((response) => response.json())
+            .then((json) => {
+                this.setState ({
+                    movies: json.movies,
+                    isLoaded: true,
+                })
+            })
     }
 
     render() {
-        return (
-            <Fragment>
-                <h2>Choose a Movie!</h2>
-                <ul>
-                    {this.state.movies.map((m)=>(
-                        <li key={m.id}>
-                            <Link to={`/movies/${m.id}`} >{m.title}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </Fragment>
-            
-        );
+        const { movies, isLoaded } = this.state
+        if (!isLoaded) {
+            return <p>Loading....</p>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Choose a Movie!</h2>
+                    <ul>
+                        {movies.map((m)=>(
+                            <li key={m.id}>
+                                <Link to={`/movies/${m.id}`} >{m.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </Fragment>
+                
+            );
+        }
     }
 }
