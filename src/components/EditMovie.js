626ed60e3329ed49554ad6af566629e0f6272ab6
Yuta <y.sugiyama54@gmail.com>
2022-03-03 23:20:24 +0900

    入力内容をPOSTで送信

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -33,8 +33,22 @@ class EditMovie extends Component {
     }
 
     handleSubmit = (evt) => {
-        console.log("Form was submitted");
-        EventTarget.preventDefault();
+        evt.preventDefault();
+        
+        const data = new FormData(evt.target);
+        const payload = Object.fromEntries(data.entries());
+        console.log(payload);
+
+        const requestOptions = {
+            method: 'POST',
+            body: JSON.stringify(payload)
+        }
+
+        fetch('http://localhost:4000/v1/admin/editmovie', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
     }
 
     handleChange = (evt) => {
