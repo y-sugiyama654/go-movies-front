a7ec0bc6dafb07ead076019436ed92574e62de16
Yuta <y.sugiyama54@gmail.com>
2022-03-08 23:10:40 +0900

    タイトルのバリデーションを追加

--- src/components/form-components/Input.js
+++ src/components/form-components/Input.js
@@ -3,14 +3,18 @@ const Input = (props) => {
         <div className="mb-3">
             <label htmlFor={props.name} className="form-label">{props.title}</label>
             <input 
+
                 type={props.type}
-                className="form-control"
+                className={`form-control ${props.className}`}
                 id={props.name}
                 name={props.name}
                 value={props.value}
                 onChange={props.handleChange}
                 placeholder={props.placeholder}
             />
+            <div className={props.errorDiv}>
+                {props.errorMsg}
+            </div>
         </div>  
     );
 };
